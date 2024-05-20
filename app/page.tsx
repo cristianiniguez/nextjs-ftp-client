e9952c4b1cd3025b27b3061a6e1110f6d7e7eccc
Cristian Felix Iñiguez Porcel <64929919+cristianiniguez@users.noreply.github.com>
2024-05-20 15:44:10 +0000

    installed flowbite

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,19 @@
 'use client'
 
 import { FormEventHandler, useState } from 'react'
+import Dropzone from './components/Dropzone'
+import { Button } from 'flowbite-react'
 
 export default function Home() {
   const [file, setFile] = useState<File>()
+  console.log(file)
 
   const handleSubmit: FormEventHandler = async e => {
     e.preventDefault()
     if (!file) return
 
+    console.log('Sending File')
+    
     try {
       const data = new FormData()
       data.set('file', file)
@@ -17,22 +22,20 @@ export default function Home() {
         body: data
       })
       if (!res.ok) throw new Error(await res.text())
+      console.log('File Saved')
     } catch (e) {
-      console.error(e)
+      console.error('File Not Saved', e)
     }
   }
 
   return (
-    <main>
-      <form onSubmit={handleSubmit}>
-        <input
-          type='file'
-          name='file'
-          id='file'
-          onChange={e => setFile(e.target.files?.[0])}
-        />
-        <button>Upload</button>
-      </form>
+    <main className='p-4'>
+      <div className='max-w-xl mx-auto'>
+        <form onSubmit={handleSubmit}>
+          <Dropzone name='file' onChange={e => setFile(e.target.files?.[0])} />
+          <Button className='px-4' color='blue' type='submit'>Upload</Button>
+        </form>
+      </div>
     </main>
   )
 }
